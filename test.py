12807a0112c0949081358faa963a76cc64a5ee45
anamoyee <thecreatorrr333@gmail.com>
2025-03-23 19:39:25 +0100

    Stuff is working now

---
+++ test.py
@@ -0,0 +1,20 @@
+import json
+import pathlib as p
+
+import discord_cx_pydantic as dcx
+from tcrutils.console import c
+
+PATHS = [
+	p.Path(__file__).parent / "gitignored/smallest.json",
+	p.Path(__file__).parent / "gitignored/small.json",
+	p.Path(__file__).parent / "gitignored/furaski.json",
+	(p.Path(__file__).parent / "gitignored/hikari").glob("*.json").__next__(),
+	p.Path(__file__).parent / "gitignored/forum/private.json",
+	p.Path(__file__).parent / "gitignored/forum/public.json",
+	p.Path(__file__).parent / "gitignored/forum/pub-thread.json",
+]
+
+for path in PATHS:
+	export = dcx.Export.from_path(path)
+
+	c(export, depth_limit=2)
